f8c1d8f70d5686f6ef4a2bdc1b34cd4215864c8e
Deckry <95526592+DeckryZ@users.noreply.github.com>
2025-12-06 15:44:17 +0800

    Update lihkg.js

--- lihkg.js
+++ lihkg.js
@@ -1,25 +1,27 @@
-
-
 var body = JSON.parse($response.body);
+
 
 if (body.response && body.response.me) {
     body.response.me.is_plus_user = true;
     body.response.me.plus_expiry_time = 9999999999;
-
 }
-
 
 if (body.response && body.response.items) {
     body.response.items.forEach(function(item) {
         item.display_vote = true;
     });
+}
+
 
 if (body.response && body.response.item_data) {
+    // 情况 A: item_data 是一个数组 (通常是评论列表)
     if (Array.isArray(body.response.item_data)) {
         body.response.item_data.forEach(function(item) {
             item.display_vote = true;
         });
-    } else {
+    } 
+   
+    else {
         body.response.item_data.display_vote = true;
     }
 }
