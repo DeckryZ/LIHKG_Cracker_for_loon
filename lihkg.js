e9f306fa9b9d5d1281e654c1ff233e0d7245cac0
Deckry <95526592+DeckryZ@users.noreply.github.com>
2025-12-06 18:04:22 +0800

    Add vote display toggle based on argument

--- lihkg.js
+++ lihkg.js
@@ -1,33 +1,46 @@
 
-
 var body = JSON.parse($response.body);
 
-if (body.response && body.response.me) {
-    body.response.me.is_plus_user = true;
-    body.response.me.plus_expiry_time = new Date(9876, 4, 4, 3, 21, 0).getTime() / 1000;
-}
 
+var enableVote = true;
 
-if (body.response && body.response.items) {
-    body.response.items.forEach(function(item) {
-        item.display_vote = true;
-    });
-}
+if (typeof $argument !== "undefined") {
 
-
-if (body.response && body.response.item_data) {
-    if (Array.isArray(body.response.item_data)) {
-        body.response.item_data.forEach(function(item) {
-            item.display_vote = true;
-        });
-    } else {
-        body.response.item_data.display_vote = true;
+    if ($argument.indexOf("show_votes=false") !== -1) {
+        enableVote = false;
     }
 }
 
 
-if (body.response && typeof body.response.display_vote !== "undefined") {
-    body.response.display_vote = true;
+if (body.response && body.response.me) {
+    body.response.me.is_plus_user = true;
+    // 9876年5月4日 03:21:00
+    body.response.me.plus_expiry_time = new Date(9876, 4, 4, 3, 21, 0).getTime() / 1000;
+}
+
+
+if (enableVote) {
+    
+
+    if (body.response && body.response.items) {
+        body.response.items.forEach(function(item) {
+            item.display_vote = true;
+        });
+    }
+
+    if (body.response && body.response.item_data) {
+        if (Array.isArray(body.response.item_data)) {
+            body.response.item_data.forEach(function(item) {
+                item.display_vote = true;
+            });
+        } else {
+            body.response.item_data.display_vote = true;
+        }
+    }
+
+    if (body.response && typeof body.response.display_vote !== "undefined") {
+        body.response.display_vote = true;
+    }
 }
 
 $done({ body: JSON.stringify(body) });
